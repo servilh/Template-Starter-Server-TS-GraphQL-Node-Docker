1e2612a2115b310a4d23e425099c6d4087331444
Sergio Villaescusa <svilla@skyot.net>
2022-09-27 10:05:13 +0200

    resolver get game

--- src/graphql/resolvers/gamesResolver.ts
+++ src/graphql/resolvers/gamesResolver.ts
@@ -1,21 +1,63 @@
-import {Arg, Mutation, Query, Resolver} from 'type-graphql';
+import { Arg, Field, InputType, Mutation, Query, Resolver } from 'type-graphql';
 import "reflect-metadata";
+import Game from '../../../../common/src/models/game';
+import { ObjectId } from "mongodb";
+import { collections } from "../../services/database.service";
+
+@InputType()
+class GameInput {
+    @Field()
+    name!: string;
+    @Field()
+    price!: number;
+    @Field()
+    category!: string;
+    @Field({ nullable: true })
+    id!: string;
+}
 
 @Resolver()
 export class GameResolver {
-    
-    @Query(() => String)
-    getAll() {
-        return "alll Games";
+
+    @Query(() => [Game])
+    async getGames() {
+        return (await collections.games?.find({}).toArray());
+
     }
 
-    @Mutation(() => Boolean)
-    createGame(
-        @Arg("name") name: string, 
-        @Arg("price") price: number, 
-        @Arg("category") category: string
-    ){
-        console.log(name, category);
-        return true;
+    @Query(() => Game)
+    async getGame(@Arg("id") id: string) {
+        const query = { _id: new ObjectId(id) };
+        return (await collections.games?.findOne(query));
+    }
+
+    @Mutation(() => String)
+    async createGame(
+        @Arg("game") game: GameInput
+    ) {
+        const result = await collections.games?.insertOne(game);
+
+        return result?.insertedId;
+    }
+
+    @Mutation(() => String)
+    async upsertGame(
+        @Arg("game") game: GameInput
+    ) {
+       
+        if (game.id) {
+            const query = { _id: new ObjectId(game.id) };
+            const result = await collections.games?.updateOne(query, {
+                $set: {
+                    name: game.name,
+                    category: game.category,
+                    price: game.price
+                }
+            });
+            return game.id;
+        } else {
+            const result = await collections.games?.insertOne(game);
+            return result?.insertedId;
+        }
     }
 }
